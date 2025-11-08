820c1a4d16fa1f93fc46fb728dfec701c47507f5
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-08 20:50:28 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-dnf install ansible -y 
-ansible-pull -u https://github.com/chaitanyakrishnadevops1-blip/ansible-roboshop-roles-tf.git -e component=$component mongodb main.yaml
-
-
+dnf install -y ansible git
+ansible-pull -u https://github.com/chaitanyakrishnadevops1-blip/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
