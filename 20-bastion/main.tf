695dbc61d40b6265add2d890b7e3075b00779d2f
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-12 11:38:18 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "bastion" {
   ami = local.ami_id
   instance_type = "t3.micro"
+  root_block_device {
+    volume_size = 50
+  }
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
   tags = merge(
