e097c5bc7cc155fdc3a6c9a758cffebbc651618a
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-12 19:29:07 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -78,14 +78,129 @@ resource "aws_instance" "redis" {
   }
   # terraform copies this file to mongodb server
   provisioner "remote-exec" {
-     inline = [ 
+    inline = [ 
         "chmod +x /tmp/bootstrap.sh",
         "sudo sh  /tmp/bootstrap.sh redis"
     ]
   }
 }
 
+resource "aws_instance" "rabbitmq" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id = local.database_subnet_id
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-rabbitmq" # roboshop-dev-rabbitmq
 
+    }
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id,
+]
+
+
+   connection {
+     type = "ssh"
+     user = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.rabbitmq.private_ip
+  
+  }
+    
+  provisioner "file" {
+      source = "bootstrap.sh"
+      destination = "/tmp/bootstrap.sh"
+    
+  }
+  # terraform copies this file to mongodb server
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh  /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
+
+
+resource "aws_instance" "mysql" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_id
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mysql
+
+    }
+  )
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id,
+]
+
+
+   connection {
+     type = "ssh"
+     user = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.mysql.private_ip
+  
+  }
+    
+  provisioner "file" {
+      source = "bootstrap.sh"
+      destination = "/tmp/bootstrap.sh"
+    
+  }
+  # terraform copies this file to mongodb server
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh  /tmp/bootstrap.sh mysql"
+    ]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+ 
 
 
 
