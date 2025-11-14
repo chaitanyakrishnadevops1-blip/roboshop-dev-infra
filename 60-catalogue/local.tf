f0ca09955900a5abd3b84ef379c705fe83f2fa3f
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-14 21:00:13 +0530

    terraform

---
+++ 60-catalogue/local.tf
@@ -0,0 +1,12 @@
+locals{
+    common_name_suffix = "${var.project_name}-${var.environment}"
+   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    ami_id = data.aws_ami.joindevops.id
+    common_tags = {
+        project = var.project_name
+        Environment = var.environment
+        Terraform = "true"
+    }
+}
+
+
