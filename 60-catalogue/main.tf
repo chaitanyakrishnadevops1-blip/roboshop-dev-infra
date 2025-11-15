768af2a089cf75783bd5e94d8169141025ee2c15
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-15 11:25:30 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "catalogue" {
      inline = [ 
         "chmod +x /tmp/catalogue.sh",
         # "sudo sh /tmp/catalogue.sh"
-        "sudo sh /tmp/catalogue.sh catalogue"
+        "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
         
     ]
   }
