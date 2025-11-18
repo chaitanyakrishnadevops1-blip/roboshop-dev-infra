5078311594710b6263ccae68cbe961ab21cac064
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-18 11:39:20 +0530

    terraform

--- 60-catalogue/local.tf
+++ 60-catalogue/local.tf
@@ -1,6 +1,6 @@
 locals{
     common_name_suffix = "${var.project_name}-${var.environment}"
-   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     ami_id = data.aws_ami.joindevops.id
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     common_tags = {
