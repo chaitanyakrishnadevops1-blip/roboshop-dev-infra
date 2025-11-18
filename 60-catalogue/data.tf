da5d7102e3265f414c1404ae269a834eb869b070
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-18 11:43:18 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -24,8 +24,8 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   
 }
 
-data "aws_ssm_parameter" "catalogue_subnet_id" {
-  name = "/${var.project_name}/${var.environment}/catalogue_subnet_id"
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
   
 }
 
