8a88884321ff28fa886f210ebfaf0b758581e4a5
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-11-19 16:50:03 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -61,6 +61,14 @@ resource "aws_security_group_rule" "catalogue-bastion" {
   to_port           = 22
 }
 
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.catalogue_sg_id
+  from_port         = 27017
+  protocol          = "tcp"
+  to_port           = 27017
+}
 
 
 
